1c898e349cf7f205f594d21e7425498738d82f66
NazarSavchuk <nazarsavcuk3@gmail.com>
2022-09-13 23:23:18 +0200

    init: localStorage cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,11 +11,20 @@ function Header() {
   const { items, totalPrice } = useSelector(selectCart);
 
   const location = useLocation();
+  const isMounted = React.useRef(false);
 
   const totalCount = items.reduce(
     (sum: number, item: any) => sum + item.count,
     0
   );
+
+  React.useEffect(() => {
+    if (isMounted.current) {
+      const json = JSON.stringify(items);
+      localStorage.setItem("cart", json);
+    }
+    isMounted.current = true;
+  }, [items]);
 
   return (
     <div className="header">
@@ -29,7 +38,7 @@ function Header() {
             </div>
           </div>
         </Link>
-        <Search />
+        {location.pathname !== "/cart" && <Search />}
         <div className="header__cart">
           {location.pathname !== "/cart" && (
             <Link to="/cart" className="button button--cart">
